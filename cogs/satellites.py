6419bb3ceb3fb75207ca59c00c0205405692b840
chillerno1 <gh.chiller@pm.me>
2022-03-22 09:25:26 +1100

    add content_type param to response

--- cogs/satellites.py
+++ cogs/satellites.py
@@ -33,7 +33,7 @@ async def get_opensea_floor_price(url: str, alias: str) -> Union[FloorPrice, boo
     try:
         async with aiohttp.ClientSession() as session:
             async with session.get(url) as response:
-                content = await response.json()
+                content = await response.json(content_type=None)
                 if content:
                     price = content['collection']['stats']['floor_price']
                     return FloorPrice(source=source, price=f"{price} ETH", project=alias)
